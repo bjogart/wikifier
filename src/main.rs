ea5cffa39104d17b4dc1cbed789eb885b50d8081
Jasper van den Bogart <bjogart>
2022-09-18 20:38:39 +0200

    clean up main

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use std::fs;
 use std::fs::read_dir;
 use std::fs::DirEntry;
 use std::io::Error;
+use std::path::Path;
 use std::path::PathBuf;
 
 use clap::Parser;
@@ -29,12 +30,33 @@ struct Args {
 fn main() {
     let args = Args::parse();
 
-    let inp_dir = PathBuf::from(args.input_dir)
+    let inp_dir = find_input_dir(&args.input_dir);
+    let inp_files = find_input_files(&inp_dir);
+
+    let md = MdRenderer::new(inp_dir);
+    let out_dir = find_or_make_out_dir(args.out_dir);
+
+    for file in inp_files {
+        let ext = file.extension().unwrap_or_else(|| OsStr::new("")).to_string_lossy();
+
+        if ext.to_lowercase() == MD_EXT {
+            render_md_file(&md, &out_dir, &file);
+        } else {
+            copy_file(&out_dir, &file);
+        }
+    }
+}
+
+fn find_input_dir(arg: &str) -> PathBuf {
+    PathBuf::from(arg)
         .canonicalize()
-        .map_err(|err| format!("invalid input directory: {err}"))
-        .unwrap();
+        .map_err(|err| format!("'{arg}' is not a valid input directory: {err}"))
+        .unwrap()
+}
+
+fn find_input_files(dir: &Path) -> impl Iterator<Item = PathBuf> {
     let inp_dir_read =
-        read_dir(&inp_dir).map_err(|err| format!("cannot read input directory: {err}")).unwrap();
+        read_dir(dir).map_err(|err| format!("cannot read input directory: {err}")).unwrap();
     let (inp_entries, inp_errs): (Vec<DirEntry>, Vec<Error>) =
         inp_dir_read.partition_map(|e| match e {
             Ok(e) => Either::Left(e),
@@ -49,46 +71,45 @@ fn main() {
         }
     }
 
-    let inp_files = inp_entries.into_iter().filter_map(|e| {
+    inp_entries.into_iter().filter_map(|e| {
         let path = e.path();
         path.is_file().then(|| path)
-    });
+    })
+}
 
-    let out_dir = PathBuf::from(args.out_dir);
+fn find_or_make_out_dir(arg: String) -> PathBuf {
+    let out_dir = PathBuf::from(arg);
     fs::create_dir_all(&out_dir).unwrap();
-    let out_dir = out_dir.canonicalize().unwrap();
+    out_dir.canonicalize().unwrap()
+}
 
-    let md = MdRenderer::new(inp_dir);
+fn render_md_file(md: &MdRenderer, out_dir: &Path, file: &Path) {
+    let dest = out_dir.join(file.with_extension(HTML_EXT).file_name().unwrap());
+    println!("render '{}' to '{}'", file.display(), dest.display());
 
-    for file in inp_files {
-        let ext = file.extension().unwrap_or_else(|| OsStr::new("")).to_string_lossy();
-        if ext.to_lowercase() == MD_EXT {
-            let dest = out_dir.join(file.with_extension(HTML_EXT).file_name().unwrap());
-            println!("render '{}' to '{}'", file.display(), dest.display());
+    match fs::read_to_string(&file) {
+        Err(err) => {
+            let msg = format!("could not read '{}': {err}", file.display()).red();
+            eprintln!("{msg}");
+        }
+        Ok(source) => {
+            let res = md.render(&source);
 
-            match fs::read_to_string(&file) {
-                Err(err) => {
-                    let msg = format!("could not read '{}': {err}", file.display()).red();
-                    eprintln!("{msg}");
-                }
-                Ok(source) => {
-                    let res = md.render(&source);
-
-                    if let Err(err) = fs::write(&dest, res) {
-                        let msg = format!("could not write to '{}': {err}", dest.display()).red();
-                        eprintln!("{msg}");
-                    }
-                }
-            }
-        } else {
-            let dest = out_dir.join(file.file_name().unwrap());
-            let cp = format!("'{}' to '{}'", file.display(), dest.display());
-            println!("copy {cp}");
-
-            if let Err(err) = fs::copy(file, dest) {
-                let msg = format!("cannot copy {cp}: {err}").red();
+            if let Err(err) = fs::write(&dest, res) {
+                let msg = format!("could not write to '{}': {err}", dest.display()).red();
                 eprintln!("{msg}");
             }
         }
+    }
+}
+
+fn copy_file(out_dir: &Path, file: &Path) {
+    let dest = out_dir.join(file.file_name().unwrap());
+    let cp = format!("'{}' to '{}'", file.display(), dest.display());
+    println!("copy {cp}");
+
+    if let Err(err) = fs::copy(file, dest) {
+        let msg = format!("cannot copy {cp}: {err}").red();
+        eprintln!("{msg}");
     }
 }
